2c3305b3c493f10e27a37041ccac8462ab9fc2c8
Jobayer Ahmed <Jobayer6735@gmail.com>
2023-12-05 09:15:54 +0600

    - Fix error handling issue

--- src/module/reward/reward.controller.ts
+++ src/module/reward/reward.controller.ts
@@ -1,18 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpException, HttpStatus, Post } from '@nestjs/common';
 import { Reward } from '../../entities/Reward';
 import { AddRewardDto } from './dto/add-reward.dto';
 import { RewardService } from './reward.service';
 
 @Controller('reward')
 export class RewardController {
-  constructor(private readonly rewardService: RewardService) {}
+  constructor(private readonly rewardService: RewardService) { }
 
   @Post()
   async addReward(@Body() payload: AddRewardDto): Promise<Reward> {
     try {
       return this.rewardService.addReward(payload);
     } catch (error) {
-      throw new Error(`Failed to save reward due to ${error?.message}`);
+      throw new HttpException(error?.message, HttpStatus.BAD_REQUEST);
     }
   }
 }
